00179067f41bcf21c1d0e690389cfa3bb812e331
leeofri <leeofri@gmail.com>
2015-07-18 07:53:54 -0700

    finish  of the location manager befor checks

--- Managers/LocalizationManager.h
+++ Managers/LocalizationManager.h
@@ -3,6 +3,12 @@
 #include <vector>
 #include "../Particle.h"
 #include "../Models/Location.h"
+#include <float.h>
+
+// defind
+#define NUMBER_OF_PARTICLE 100
+#define TRASHHOLE 0.5
+#define BEST_EFFORT 0.9
 
 using namespace std;
 
@@ -12,17 +18,22 @@ class LocalizationManager {
 
 
 public:
+	// Data Member
 	Location currLocation;
+	Map* m_Map;
+
+	// Function
+	void upDate(float deltaX, float deltaY, float deltaYaw, float laserArr[]);
+	Location BestLocation();
+
 protected:
 	// Data Member
 	std::list<Particle> particleList;
 
-
 	// Ctor
-	LocalizationManager(Location currLocation);
+	LocalizationManager(Location currLocation,  Map* currMap);
 
-	// Function
-	void update (float deltaX, float deltaY, float deltaYaw, float laserArr[]);
-
+	// Dtor
+	~LocalizationManager();
 	};
 }
