64c8c5cc0c29dc70a7404fcd4b9c519afee6e34f
Gal Schlezinger <galstar@spitfire.co.il>
2015-07-18 13:27:59 -0700

    fixes :star:

--- Managers/LocalizationManager.cpp
+++ Managers/LocalizationManager.cpp
@@ -1,74 +1,74 @@
 #include "LocalizationManager.h"
 
-
-
 namespace Managers {
 
-LocalizationManager::LocalizationManager(Location currLocation, AnotherMap* currMap)
-{
+LocalizationManager::LocalizationManager(Location currLocation,
+		AnotherMap* currMap) {
 	// add the location
 	this->currLocation = Location(currLocation);
 	this->particleList = std::list<Particle>();
 
 	// firest particle
-	Particle fatherParticle = Particle(this->currLocation.m_X,this->currLocation.m_Y,this->currLocation.m_Yaw,1);
+	Particle fatherParticle = Particle(this->currLocation.m_X,
+			this->currLocation.m_Y, this->currLocation.m_Yaw, 1);
 
 	// give the father particle the map
 	fatherParticle.SetMap(this->m_Map);
 
 	// rando all the particle list
-	for (unsigned i=0; i <= NUMBER_OF_PARTICLE; i++)
-	{
-		this->particleList.push_front(*(fatherParticle.CreateChild(PARTICALE_RADIOS_FROM_ROBOT,PARTICAL_DGREE_YAW_FROM_ROBOT)));
+	for (unsigned i = 0; i <= NUMBER_OF_PARTICLE; i++) {
+		this->particleList.push_front(
+				*(fatherParticle.CreateChild(PARTICALE_RADIOS_FROM_ROBOT,
+						PARTICAL_DGREE_YAW_FROM_ROBOT)));
 	}
 
 }
 
-void LocalizationManager::upDate(float deltaX, float deltaY, float deltaYaw, float laserArr[])
-{
+void LocalizationManager::upDate(float deltaX, float deltaY, float deltaYaw,
+		float laserArr[]) {
 	//creat new chileds
 	vector<list<Particle>::iterator> BestParticle;
+	vector<list<Particle>::iterator> kakis;
 	unsigned itCount = 0;
 	cout << this->particleList.size() << endl;
 
 	// update and delete
-	for (std::list<Particle>::iterator it = this->particleList.begin(); it != this->particleList.end(); it++)
-	{
-		it->Update(deltaX,deltaY,deltaYaw,laserArr);
-		if (it->belief < TRASHHOLE)
-		{
-			this->particleList.erase(it);
-		}
-		else if (it->belief > BEST_EFFORT)
-		{
+	for (std::list<Particle>::iterator it = this->particleList.begin();
+			it != this->particleList.end(); it++) {
+		it->Update(deltaX, deltaY, deltaYaw, laserArr);
+		if (it->belief < TRASHHOLE) {
+			kakis.push_back(it);
+		} else if (it->belief > BEST_EFFORT) {
 			BestParticle.push_back(it);
 		}
 	}
 
+	for (std::list<Particle>::iterator it : kakis) {
+		this->particleList.erase(it);
+	}
+
 	// born new children
-	for (unsigned i = 0; i < NUMBER_OF_PARTICLE - this->particleList.size(); i++)
-	{
-		this->particleList.push_front((*(BestParticle[i%BestParticle.size()])->CreateChild(PARTIACLE_CHILED_RADIOS_CM,PARTICAL_DGREE_YAW)));
+	for (unsigned i = 0; i < NUMBER_OF_PARTICLE - this->particleList.size();
+			i++) {
+		this->particleList.push_front(
+				(*(BestParticle[fmod(i, BestParticle.size())])->CreateChild(
+						PARTIACLE_CHILED_RADIOS_CM, PARTICAL_DGREE_YAW)));
 	}
 }
 
-Location LocalizationManager::BestLocation()
-{
+Location LocalizationManager::BestLocation() {
 	Particle best = this->particleList.front();
-	for (Particle p : this->particleList)
-	{
-		if (best.belief < p.belief)
-		{
+	for (Particle p : this->particleList) {
+		if (best.belief < p.belief) {
 			best = p;
 		}
 	}
 
-	return (Location(best.x,best.y,best.yaw));
+	return (Location(best.x, best.y, best.yaw));
 }
 
-LocalizationManager::~LocalizationManager()
-{
+LocalizationManager::~LocalizationManager() {
 	this->currLocation.~Location();
 	this->particleList.~list();
-	}
+}
 }
